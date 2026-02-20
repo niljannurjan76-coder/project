070b9af359ac2041d7e222725d4106fee940acaa
niljannurjan76-coder <niljannurjan76@gmail.com>
2026-02-21 00:56:13 +0500

    cw2 05.02

--- js.js
+++ js.js
@@ -1,17 +1,4 @@
-let count = 0;
-
-const minusBtn = document.getElementById("minus");
-const plusBtn = document.getElementById("plus");
-const countEl = document.getElementById("count");
-
-plusBtn.addEventListener("click", () => {
-  count++;
-  countEl.textContent = count;
-});
-
-minusBtn.addEventListener("click", () => {
-  if (count > 0) {
-    count--;
-    countEl.textContent = count;
-  }
-});
+function toggleTheme() {
+            const body = document.body;
+            body.classList.toggle('light-mode');
+        }
